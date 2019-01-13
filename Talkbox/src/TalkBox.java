d49723020d3ba5e0ace345ced588000efee40df0
James Kong <j25kong@gmail.com>
2019-01-13 18:34:52 -0500

    James testing

--- Talkbox/src/TalkBox.java
+++ Talkbox/src/TalkBox.java
@@ -4,6 +4,7 @@ public class TalkBox {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.print("Fun with Git-two");
+			System.out.println("James Testing");
 	}
 
 }
