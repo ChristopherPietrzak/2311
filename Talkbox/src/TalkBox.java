04136474efd34d8abf70ef8051663c4045ae4b6b
James Kong <j25kong@gmail.com>
2019-01-20 11:27:39 -0500

    adding my working branch

--- Talkbox/src/TalkBox.java
+++ Talkbox/src/TalkBox.java
@@ -3,10 +3,7 @@ public class TalkBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.print("Fun with Git-two");
-			System.out.println("James Testing");
-			System.out.println("Testing Thomas");
-			System.out.println("it works");
+			System.out.print("James Branch");
 			
 	}
 
