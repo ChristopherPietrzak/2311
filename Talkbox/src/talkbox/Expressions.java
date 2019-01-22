79629bd9b9d161d23cd6ae87f57b1c025ee8e201
Thomas <Thomas@DESKTOP-8VILD4H>
2019-01-22 10:51:09 -0500

    added buttons and espressions as well as testing their function in the preset library

---
+++ Talkbox/src/talkbox/Expressions.java
@@ -0,0 +1,47 @@
+package talkbox;
+import java.util.*;
+
+public class Expressions 
+{
+	private int buttonNum;
+	private int audioButtonNum;
+	private ArrayList buttonList;
+	
+	public Expressions(int inButtonNum)//, int inAudioButtonNum)
+	{
+		buttonNum = inButtonNum;
+		//audioButtonNum = inAudioButtonNum;
+		buttonList = new ArrayList<Button>();
+		audioButtonNum = buttonList.size();
+	}
+	public void setButtonNum(int input)
+	{
+		buttonNum = input;
+	}
+	public void setAudioButtonNum(int input)
+	{
+		audioButtonNum = input;
+	}
+	public int getButtonNum()
+	{
+		return buttonNum;
+	}
+	public int getAudioButtonNum()
+	{
+		return buttonList.size(); //audioButtonNum;
+	}
+	public void AddButton(Button input)
+	{
+		buttonList.add(input);
+	}
+	public void AddButton(String name, String audioFile, String iconFile) 
+	{
+		buttonList.add(new Button(name, audioFile, iconFile));
+	}
+	public void RemoveButton(Button input)
+	{
+		buttonList.remove(input);
+	}
+	
+
+}
